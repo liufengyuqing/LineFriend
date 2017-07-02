e528db948a7bee73830f107572531febdfb5b22a
liuzhiwei <2014012566@nwsuaf.edu.cn>
2017-07-02 22:22:26 +0800

    fixed bug

---
+++ src/cn/edu/nwsuaf/interceptor/FielInterceptor.java
@@ -0,0 +1,14 @@
+/**
+ * author 刘志伟
+ * 2017-6-29
+ */
+package cn.edu.nwsuaf.interceptor;
+
+/**
+ * @author 刘志伟
+ *
+ * 2017-6-29
+ */
+public class FielInterceptor {
+
+}
---
+++ src/cn/edu/nwsuaf/interceptor/FileTypeInterceptor.java
@@ -0,0 +1,15 @@
+/**
+ * author 刘志伟
+ * 2017-6-29
+ */
+package cn.edu.nwsuaf.interceptor;
+
+/**
+ * @author 刘志伟
+ *
+ * 2017-6-29
+ */
+public class FileTypeInterceptor {
+	
+
+}
